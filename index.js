d5f2932b40992e172648a628a1e80925ef58af53
anrokk <anrokk@taltech.ee>
2024-07-06 17:08:10 +0300

    index.html optimization

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 document.getElementById("h2").textContent = 'Firmast';
+document.getElementById("firmast").textContent = 'Tecnofood Systems OÜ on 2005 aastal loodud toiduainetetööstuse seadmete müüki ja hooldust teostav ettevõte. Firma on arenenud kiiresti tänu suurepärasele koostööle klientidega. Missiooniks on olla paindlik koostööpartner ja leida alati professionaalne lahendus klientide rahuloluks.'
+document.getElementById("esindame").textContent = ' Meie esindame järgmisi omal alal tuntud firmasid:'
+
+let fullName = "Andreas Rokk";
+let age = 20;
+let student = false;
