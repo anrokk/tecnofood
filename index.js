6e65b64dfeec2add65e7d39fd9dcebd0f4d2a114
anrokk <anrokk@taltech.ee>
2024-07-07 14:42:55 +0300

    test

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // firmast
 
-document.getElementById("firmast").textContent = 'Firmast';
+document.getElementById("firmast").textContent = 'Firmat';
 document.getElementById("firmast_tekst").textContent = 'Tecnofood Systems OÜ on 2005 aastal loodud toiduainetetööstuse seadmete müüki ja hooldust teostav ettevõte. Firma on arenenud kiiresti tänu suurepärasele koostööle klientidega. Missiooniks on olla paindlik koostööpartner ja leida alati professionaalne lahendus klientide rahuloluks.'
 document.getElementById("esindame_tekst").textContent = 'Meie esindame järgmisi omal alal tuntud firmasid:'
 
